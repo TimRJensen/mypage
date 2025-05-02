4230ac2752b786e322f66493c32c2ea4df4c1017
Tim R. Jensen <timrjensen@gmail.com>
2025-05-02 18:07:35 +0200

    FIX: wups It usually helps to point the server to the right file

--- app/src/server.ts
+++ app/src/server.ts
@@ -16,10 +16,11 @@ async function handleHTTP(req: Request): Promise<Response> {
     await build();
 
     const userAgent = req.headers.get("User-Agent")?.toLowerCase() || "";
-    const index = /googlebot|bingbot|duckduckbot/i.test(userAgent) ? "crawler.html" : "index.html"
+    const index = /googlebot|bingbot|duckduckbot/i.test(userAgent) ? "/crawler.html" : "/index.html"
 
     const url = new URL(req.url);
     const path = Deno.cwd() + (url.pathname === "/" ? index : decodeURI(url.pathname));
+
     try {
         const file = await Deno.readFile(path);
         const ext = path.split(".").pop() || "txt";
